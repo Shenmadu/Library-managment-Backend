bedd1fdb07096346447872c89badb340c3650805
shenmadu <tashen.madu@gmail.com>
2024-01-18 21:09:28 +0530

    Add PostMapping

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import org.example.dto.Book;
 import org.example.service.BookSevice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     @Autowired
     BookSevice sevice;
-
+@PostMapping
     public void addBook(Book book){
-
+        sevice.addBook(book);
     }
 
 }
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -2,7 +2,9 @@ package org.example.service.impl;
 
 import org.example.dto.Book;
 import org.example.service.BookSevice;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookSevice {
     @Override
     public void addBook(Book book) {
